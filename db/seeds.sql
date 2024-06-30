58bd872310dccc69c10c2f2dd606095d61ab06a9
Laptop <jdkyaz@gmail.com>
2024-06-30 05:49:16 -0700

    added more employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,26 @@
 USE employee_tracker;
 
-INSERT INTO department (name) VALUES
+INSERT INTO department (name)
+VALUES
 ('Sales'),
 ('Engineering'),
 ('Finance'),
 ('Legal');
 
-INSERT INTO role (title, salary, department_id) VALUES
+INSERT INTO role (title, salary, department_id) 
+VALUES
 ('Sales Lead', 100000, 1),
 ('Salesperson', 80000, 1),
 ('Software Engineer', 120000, 2),
 ('Accountant', 90000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES
 ('John', 'Doe', 1, NULL),
 ('Willy', 'Wonka', 2, 1),
 ('Jaden', 'Dulesky', 3, NULL),
 ('Joe', 'Mama', 4, NULL);
+('Tom', 'Brown', 1, 1),
+('Emma', 'Jones', 2, 2),
+('Chris', 'Davis', 3, 3),
+('Anna', 'Miller', 4, 4);
